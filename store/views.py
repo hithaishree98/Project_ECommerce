a8729fb7fedb28338ebe65beec26fd4915ef207c
harshitha-venkatesh-2204 <harshithavenkatesh2204@gmail.com>
2023-11-29 22:18:41 -0500

    final2

--- store/views.py
+++ store/views.py
@@ -20,6 +20,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
+from .forms import ShippingAddressForm
 
 
 def product(request):
